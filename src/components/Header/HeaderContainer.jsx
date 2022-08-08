0b3988af3be2f47e206c9905b86fb8981f28d969
MarusikSuper <gouliaieva@gmail.com>
2022-08-08 13:57:19 +0200

    77 lesson (redux form)

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import {connect} from "react-redux";
-import {getAuthUserData, setAuthUserData} from "../../redux/authReducer";
+import {getAuthUserData} from "../../redux/authReducer";
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -23,14 +23,32 @@ const authReducer = (state = initialState, action) => {
     }
 }
 
-export const setAuthUserData = (userId, login, email) => ({type: SET_USER_DATA, data: {userId, login, email}});
+export const setAuthUserData = (userId, login, email, isAuth) => ({type: SET_USER_DATA, data: {userId, login, email, isAuth}});
 
 export const getAuthUserData = () => (dispatch) => {
         authAPI.getAuthData()
             .then(response => {
                 if (response.data.resultCode === 0) {
                     let {id, login, email} = response.data.data;
-                    dispatch(setAuthUserData(id, login, email));
+                    dispatch(setAuthUserData(id, login, email, true));
+                }
+            });
+}
+
+export const logIn = (email, password, rememberMe) => (dispatch) => {
+        authAPI.logIn(email, password, rememberMe)
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    dispatch(getAuthUserData());
+                }
+            });
+}
+
+export const logOut = () => (dispatch) => {
+        authAPI.logOut()
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    dispatch(setAuthUserData(null, null, null, false));
                 }
             });
 }
