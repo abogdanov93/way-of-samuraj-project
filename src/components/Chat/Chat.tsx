a6f7448cd8daae489b51b8d3e77abe488fbe303b
mariabogdanova <ShabatShalom21@>
2022-10-22 22:55:39 +0200

    MyTextarea placeholder refactored

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,11 +1,12 @@
-import React, {FC, useEffect, useRef} from "react"
+import React, {FC, useEffect} from "react"
 import commonStyles from "../../App.module.css"
+import style from "./Chat.module.css"
 import {ChatMessages} from "./ChatMessages/ChatMessages"
 import {ChatMessageForm} from "./ChatMessageForm/ChatMessageForm"
-import {useDispatch, useSelector} from "react-redux";
-import {startMessagesListening, stopMessagesListening} from "../../redux/reducers/chatReducer";
-import {AnyAction} from "redux";
-import {getStatus} from "../../redux/selectors/chatSelectors";
+import {useDispatch, useSelector} from "react-redux"
+import {startMessagesListening, stopMessagesListening} from "../../redux/reducers/chatReducer"
+import {AnyAction} from "redux"
+import {getStatus} from "../../redux/selectors/chatSelectors"
 
 const Chat: FC = () => {
     const status = useSelector(getStatus)
@@ -17,7 +18,7 @@ const Chat: FC = () => {
         }
     }, [])
 
-    return <div className={commonStyles.whiteBlock}>
+    return <div className={`${style.chat} ${commonStyles.whiteBlock}`}>
         {status === "error" && <div>Some error occurred. Please refresh the page.</div>}
         <ChatMessages/>
         <ChatMessageForm/>
